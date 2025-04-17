9d07deeebf15679b54411c78cce33c9ae1ab96f5
sheikhahmerali75@gmail.com <mohammadsaadali786.92@gmail.com>
2025-04-17 20:10:42 +0500

    stacked and Wrap

--- lib/components/message_tile.dart
+++ lib/components/message_tile.dart
@@ -3,3 +3,13 @@ import 'package:flutter/material.dart';
 Widget messageTile(String username){
   return ListTile(leading: CircleAvatar(backgroundColor: Colors.amber,),title: Text(username),subtitle: Text("Hi....."),trailing: Icon(Icons.call),);
 }
+
+class MessageTile extends StatelessWidget {
+  final username;
+   MessageTile({super.key,required this.username});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(leading: CircleAvatar(backgroundColor: Colors.amber,),title: Text(username),subtitle: Text("Hi....."),trailing: Icon(Icons.call),);
+  }
+}
--- lib/views/home.dart
+++ lib/views/home.dart
@@ -6,7 +6,35 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(body: Column(children: [ messageTile("shahzeb"),messageTile("huzaifa"),messageTile("ali"),messageTile("ahmed")]));
+    return Scaffold(
+      body: Column(
+        children: [
+          Wrap(
+            spacing: 23.33,
+            children: [
+              Container(color: Colors.yellow, width: 200, height: 29),
+              Container(color: Colors.red, width: 200, height: 29),
+              Container(color: Colors.green, width: 200, height: 29),
+              Container(color: Colors.blue, width: 200, height: 29),
+              Container(color: Colors.purple, width: 200, height: 29),
+            ],
+          ),
+          Stack(
+            alignment: Alignment.bottomRight,
+            children: [
+              Container(color: Colors.yellow, width: 200, height: 200),
+              Container(color: Colors.red, width: 100, height: 100),
+              Container(color: Colors.green, width: 50, height: 100),
+            ],
+          ),SizedBox(height: 50,),  Stack(
+            children: [
+              Container(color: Colors.yellow, width: 200, height: 200),
+              Positioned(bottom: 0,left: 0,child: Container(color: Colors.red, width: 100, height: 100)),
+              Positioned(top: 0,right: 10,child: Container(color: Colors.green, width: 50, height: 100)),
+            ],
+          ),
+        ],
+      ),
+    );
   }
 }
-
